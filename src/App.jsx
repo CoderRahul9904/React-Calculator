3f3c455ee697d6c56673ec16a4b6fb1c698fc8cd
CoderRahul9904 <mouryarahul543@gmail.com>
2024-02-29 00:45:22 +0530

    Oooo Oooo Ooo

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,26 @@ import Buttons from './components/Button.jsx'
 function App() {
   const [OperatorSelected, SetOperator] = useState('')
   function onSelectOperator(buttonsSelect) {
+    if(buttonsSelect!=='='){
     SetOperator((prevOperator) => prevOperator + buttonsSelect);
-    // SetOperator(buttonsSelect)
+    }
+    else{
+      SetOperator((prevOperator) => {
+        const result = eval(OperatorSelected);
+        return result.toString();
+      })      
+    }
   }
   return (
     <div className='bg-purple-400 w-full h-screen flex items-center justify-center text-white'>
       <div className='bg-gray-300 border-4 border-purple-700 w-1/5 h-3/5 flex flex-col items-center rounded-lg'>
+        {/* <div className='flex items-center justify-end text-black rotate-90'>
+          <select>
+            <option>...</option>
+            <option>History</option>
+            <option>Clear</option>
+          </select>
+        </div> */}
         <Display display={OperatorSelected} />
         <Buttons onSelect={onSelectOperator} />
       </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,10 @@ export default function Buttons({ onSelect }) {
     return (
         <div className="grid grid-rows-4 grid-cols-4 gap-3 w-4/5 h-4/5 my-1">
             {BASIC_OF_CALCI.map((option, index) =>
-                <button key={index} className=" bg-purple-600 cursor-pointer rounded-xl flex items-center justify-center ${option === '=' && 'bg-purple-400'}" onClick={() => onSelect(option)}>{option}</button>)
+                <button key={index} className={`bg-purple-600 cursor-pointer rounded-xl flex items-center justify-center ${option === '=' ? 'bg-purple-900' : ''}`} onClick={() => onSelect(option)}>
+                    {option}
+                </button>
+            )
             }
         </div>
     )
